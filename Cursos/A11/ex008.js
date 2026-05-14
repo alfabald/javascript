6362f25c4cdda283b79e0a8540bd12fd1778f003
alfabald <alfamutarobalde@hotmail.com>
2026-05-14 20:32:29 +0200

    condições

---
+++ Cursos/A11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permetida de 60km/h. Multado!`)} // condição simples
+console.log(`Dirija sempre usando cinto de segurança!`)
+
