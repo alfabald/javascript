a4a6ddd648ae5c19b28b895a6cda0ed0cbf0ac78
alfabald <alfamutarobalde@hotmail.com>
2026-05-17 12:58:10 +0200

    repetições

---
+++ Cursos/A13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 3)
+
+
+// var c = 1
+// while (c <= 3) {
+//     console.log(`Passo ${c}`) mais usado
+//     c++
+// }
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
+
